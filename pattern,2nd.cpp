561b2ab5592e3c4ff02808dfc97d7e861ca07a22
Dheerajch9401 <122461471+Dheerajch9401@users.noreply.github.com>
2023-01-11 20:06:53 +0530

    Update pattern,2nd.cpp

--- pattern,2nd.cpp
+++ pattern,2nd.cpp
@@ -3,20 +3,15 @@
 int main()
 {
     int i, j, n;
-
-    /* Input number of rows from user */
     printf("Enter value of n: ");
     scanf("%d", &n);
 
     for(i=1; i<=n; i++)
     {
-        /* Print i number of stars */
         for(j=1; j<=i; j++)
         {
             printf("*");
         }
-
-        /* Move to next line */
         printf("\n");
     }
 
